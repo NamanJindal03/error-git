05dd1b1985804590685e21c5e530aec6908e2ebd
Naman Jindal <namanjindal113@gmail.com>
2024-09-28 10:38:56 +0530

    task till now

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+// if 
+
+// ternary operator 
+
+// conditional operators -> > < >= && || ===
+
+let a = 20;
+
+if(a > 50){
+    console.log('this block of code is executed')
+}
+else if(a <20){
+    console.log('second block of code is executed')
+}
+else{
+    console.log('ah we have reached all the possibilities')
+}
